a60f754cd3227ec006bcdaeba48fc443ed6a4756
b52 <ignacio.mellado@gmail.com>
2022-03-18 23:59:03 -0700

    Parity of 3D C header with ASM header.

--- 3d/sbuffer/GENTRI.C
+++ 3d/sbuffer/GENTRI.C
@@ -18,7 +18,7 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
 
   // Ordenamos los puntos en altura
   p3=p->P3;
-  if(p->P1->Y >= p->P2->Y)
+  if(p->P1->ImageProjection.y >= p->P2->ImageProjection.y)
   {
     p1=p->P2;
     p2=p->P1;
@@ -27,34 +27,34 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
     p1=p->P1;
     p2=p->P2;
   }
-  if(p2->Y >= p3->Y)
+  if(p2->ImageProjection.y >= p3->ImageProjection.y)
   {
     tmp=p3;
     p3=p2;
     p2=tmp;
   }
-  if(p1->Y >= p2->Y)
+  if(p1->ImageProjection.y >= p2->ImageProjection.y)
   {
     tmp=p2;
     p2=p1;
     p1=tmp;
   }
-  p1y=ceil(p1->Y);
-  p3y=ceil(p3->Y);
+  p1y=ceil(p1->ImageProjection.y);
+  p3y=ceil(p3->ImageProjection.y);
   if((p1y>sbuffer->SLIClip.SLRR2.SLPY)||
      (p3y<sbuffer->SLIClip.SLRR1.SLPY)||
      ((p3y - p1y)<=0)) return;
 
-  p2y=ceil(p2->Y);
+  p2y=ceil(p2->ImageProjection.y);
 
-  p1x=ceil(p1->X);
-  p2x=ceil(p2->X);
-  p3x=ceil(p3->X);
+  p1x=ceil(p1->ImageProjection.x);
+  p2x=ceil(p2->ImageProjection.x);
+  p3x=ceil(p3->ImageProjection.x);
 
   // Calculamos la longitud de la linea mas ancha
-  temp = (p2->Y - p1->Y ) / (p3->Y - p1->Y );
+  temp = (p2->ImageProjection.y - p1->ImageProjection.y ) / (p3->ImageProjection.y - p1->ImageProjection.y );
 
-  if ((ancho1 = (temp * (p3->X - p1->X ) + (p1->X - p2->X))) == 0.0) return;
+  if ((ancho1 = (temp * (p3->ImageProjection.x - p1->ImageProjection.x ) + (p1->ImageProjection.x - p2->ImageProjection.x))) == 0.0) return;
 
   if(ancho1<0) ancho1--;
   else ancho1++;
@@ -63,8 +63,8 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
     // Calculamos las deltas de todas la variables
   for(i=0;i<=p->N;i++)
   {
-    sp.SPVars[i*2+1]= ancho*(temp * (p3->k[i] - p1->k[i]) + (p1->k[i] - p2->k[i]));
-    ks1[i]=   65536.0 * p1->k[i];
+    sp.SPVars[i*2+1]= ancho*(temp * (p3->SpanVars.k[i] - p1->SpanVars.k[i]) + (p1->SpanVars.k[i] - p2->SpanVars.k[i]));
+    ks1[i]=   65536.0 * p1->SpanVars.k[i];
   }
   sp.SPN=p->N+1;
   sp.SPType=p->Type;
@@ -96,19 +96,19 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
   */
   if( ancho1 > 0)
   {
-      dx2=(p3->X - p1->X)/(p3->Y - p1->Y);
-      x2=p1->X;
+      dx2=(p3->ImageProjection.x - p1->ImageProjection.x)/(p3->ImageProjection.y - p1->ImageProjection.y);
+      x2=p1->ImageProjection.x;
 
       if((p2y - p1y)!=0)
       {
-        temp=1.0/(p2->Y - p1->Y );
-        dx1=temp * (p2->X - p1->X);
+        temp=1.0/(p2->ImageProjection.y - p1->ImageProjection.y );
+        dx1=temp * (p2->ImageProjection.x - p1->ImageProjection.x);
         temp*=65536.0;
         for(i=0;i<=p->N;i++)
         {
-          deltas1[i]= temp * (p2->k[i] - p1->k[i]);
+          deltas1[i]= temp * (p2->SpanVars.k[i] - p1->SpanVars.k[i]);
         }
-        x1=p1->X;
+        x1=p1->ImageProjection.x;
         y=(int)p1y;
         if(y<sbuffer->SLIClip.SLRR1.SLPY)
         {
@@ -120,11 +120,11 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
             y=sbuffer->SLIClip.SLRR1.SLPY;
         }
 
-        x1+=((float)y-p1->Y)*dx1;
-        x2+=((float)y-p1->Y)*dx2;
+        x1+=((float)y-p1->ImageProjection.y)*dx1;
+        x2+=((float)y-p1->ImageProjection.y)*dx2;
         for(i=0;i<=p->N;i++)
         {
-          ks1[i]+=((float)y-p1->Y)*deltas1[i];
+          ks1[i]+=((float)y-p1->ImageProjection.y)*deltas1[i];
         }
 
         y2=(int)p2y;
@@ -156,10 +156,10 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
       }
       else
       {
-        x2+=((float)p1y-p1->Y)*dx2;
+        x2+=((float)p1y-p1->ImageProjection.y)*dx2;
         for(i=0;i<=p->N;i++)
         {
-          ks1[i]+=((float)p1y-p1->Y)*deltas1[i];
+          ks1[i]+=((float)p1y-p1->ImageProjection.y)*deltas1[i];
         }
       }
 
@@ -167,22 +167,22 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
       {
         for(i=0;i<=p->N;i++)
         {
-          ks1[i]=   65536.0 * p2->k[i];
+          ks1[i]=   65536.0 * p2->SpanVars.k[i];
         }
       } */
       if((p3y - p2y)==0) return;
 
-      temp=1.0/(p3->Y - p2->Y);
-      dx1=(p3->X - p2->X)*temp;
+      temp=1.0/(p3->ImageProjection.y - p2->ImageProjection.y);
+      dx1=(p3->ImageProjection.x - p2->ImageProjection.x)*temp;
       temp*=65536.0;
       for(i=0;i<=p->N;i++)
       {
-        deltas1[i]= temp * (p3->k[i] - p2->k[i]);
+        deltas1[i]= temp * (p3->SpanVars.k[i] - p2->SpanVars.k[i]);
       }
-      x1=p2->X;
+      x1=p2->ImageProjection.x;
 /*      for(i=0;i<=p->N;i++)
       {
-        ks1[i]=   65536.0 * p2->k[i]; pasado a abajo
+        ks1[i]=   65536.0 * p2->SpanVars.k[i]; pasado a abajo
       }*/
 
       y=(int)p2y;
@@ -192,11 +192,11 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
         y=sbuffer->SLIClip.SLRR1.SLPY;
         x2+=((float)y-p2y)*dx2;
       }
-      x1+=((float)y-p2->Y)*dx1;
-      //x2+=((float)y-p2->Y)*dx2;
+      x1+=((float)y-p2->ImageProjection.y)*dx1;
+      //x2+=((float)y-p2->ImageProjection.y)*dx2;
       for(i=0;i<=p->N;i++)
       {
-        ks1[i]=65536.0 * p2->k[i]+((float)y-p2->Y)*deltas1[i];
+        ks1[i]=65536.0 * p2->SpanVars.k[i]+((float)y-p2->ImageProjection.y)*deltas1[i];
       }
       y2=(int)p3y;
       if(y2>sbuffer->SLIClip.SLRR2.SLPY) y2=sbuffer->SLIClip.SLRR2.SLPY;
@@ -228,22 +228,22 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
   }
   else
   {
-      temp=1.0/(p3->Y - p1->Y);
-      dx1=temp * (p3->X - p1->X);
+      temp=1.0/(p3->ImageProjection.y - p1->ImageProjection.y);
+      dx1=temp * (p3->ImageProjection.x - p1->ImageProjection.x);
       temp*=65536.0;
       for(i=0;i<=p->N;i++)
       {
-        deltas1[i]= temp * (p3->k[i] - p1->k[i]);
+        deltas1[i]= temp * (p3->SpanVars.k[i] - p1->SpanVars.k[i]);
       }
-      dx3=(p3->X - p2->X)/(p3->Y - p2->Y);
+      dx3=(p3->ImageProjection.x - p2->ImageProjection.x)/(p3->ImageProjection.y - p2->ImageProjection.y);
 
-      x1=p1->X;
+      x1=p1->ImageProjection.x;
 
       if((p2y - p1y)!=0)
       {
-        x2=p1->X;
+        x2=p1->ImageProjection.x;
 
-        dx2=(p2->X - p1->X)/(p2->Y - p1->Y );
+        dx2=(p2->ImageProjection.x - p1->ImageProjection.x)/(p2->ImageProjection.y - p1->ImageProjection.y );
 
         y=(int)p1y;
 
@@ -259,12 +259,12 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
           }
         }
 
-        x1+=((float)y-p1->Y)*dx1;
-        x2+=((float)y-p1->Y)*dx2;
+        x1+=((float)y-p1->ImageProjection.y)*dx1;
+        x2+=((float)y-p1->ImageProjection.y)*dx2;
 
         for(i=0;i<=p->N;i++)
         {
-          ks1[i]+=((float)y-p1->Y)*deltas1[i];
+          ks1[i]+=((float)y-p1->ImageProjection.y)*deltas1[i];
         }
         y2=(int)p2y;
         if(y2>sbuffer->SLIClip.SLRR2.SLPY) y2=sbuffer->SLIClip.SLRR2.SLPY;
@@ -295,16 +295,16 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
       }
       else
       {
-        x1+=((float)p1y-p1->Y)*dx1;
+        x1+=((float)p1y-p1->ImageProjection.y)*dx1;
         for(i=0;i<=p->N;i++)
         {
-          ks1[i]+=((float)p1y-p1->Y)*deltas1[i];
+          ks1[i]+=((float)p1y-p1->ImageProjection.y)*deltas1[i];
         }
       }
 
       if((p3y - p2y)==0) return;
 
-      x2=p2->X;
+      x2=p2->ImageProjection.x;
 
       y=(int)p2y;
 
@@ -317,11 +317,11 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
           ks1[i]+=((float)y-p2y)*deltas1[i];
         }
       }
-      //x1+=((float)y-p2->Y)*dx1;
-      x2+=((float)y-p2->Y)*dx3;
+      //x1+=((float)y-p2->ImageProjection.y)*dx1;
+      x2+=((float)y-p2->ImageProjection.y)*dx3;
       /*for(i=0;i<=p->N;i++)
       {
-        ks1[i]+=((float)y-p2->Y)*deltas1[i];
+        ks1[i]+=((float)y-p2->ImageProjection.y)*deltas1[i];
       }*/
       y2=(int)p3y;
       if(y2>sbuffer->SLIClip.SLRR2.SLPY) y2=sbuffer->SLIClip.SLRR2.SLPY;
