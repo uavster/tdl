c85e27f2c7e1924bff1b834d0d4d1d063b459385
b52 <ignacio.mellado@gmail.com>
2022-03-30 23:29:11 -0700

    Prepared 3d structures for per-poly-vertex texture coordinates and lighting.

--- 3d/sbuffer/GENTRI.C
+++ 3d/sbuffer/GENTRI.C
@@ -7,6 +7,9 @@
 extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
 {
   RENDERPOINT *p1, *p2, *p3, *tmp;
+  float p1_vars[SPAN_MAX_INTERPOLATE_VARS+1];
+  float p2_vars[SPAN_MAX_INTERPOLATE_VARS+1];
+  float p3_vars[SPAN_MAX_INTERPOLATE_VARS+1];
   float deltas1[SPAN_MAX_INTERPOLATE_VARS+1];
   float ks1[SPAN_MAX_INTERPOLATE_VARS+1];
   float dx1, dx2, dx3, x1, x2;
@@ -50,7 +53,7 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
   p1x=ceil(p1->ImageProjection.x);
   p2x=ceil(p2->ImageProjection.x);
   p3x=ceil(p3->ImageProjection.x);
-
+  
   // Calculamos la longitud de la linea mas ancha
   temp = (p2->ImageProjection.y - p1->ImageProjection.y ) / (p3->ImageProjection.y - p1->ImageProjection.y );
 
@@ -60,11 +63,39 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
   else ancho1++;
   ancho=65536.0/ancho1;
 
+  // Extract interpolating variables
+  if (p->N >= 0) {
+	p1_vars[0] = p1->InverseZ;
+	p2_vars[0] = p2->InverseZ;
+	p3_vars[0] = p3->InverseZ;
+  }
+  if (p->N >= 1) {
+	p1_vars[1] = p1->Properties.TextureCoordinates.x;
+	p2_vars[1] = p2->Properties.TextureCoordinates.x;
+	p3_vars[1] = p3->Properties.TextureCoordinates.x;
+  }
+  if (p->N >= 2) {
+	p1_vars[2] = p1->Properties.TextureCoordinates.y;
+	p2_vars[2] = p2->Properties.TextureCoordinates.y;
+	p3_vars[2] = p3->Properties.TextureCoordinates.y;
+  }
+  if (p->N >= 3) {
+	p1_vars[3] = p1->Properties.Light1;
+	p2_vars[3] = p2->Properties.Light1;
+	p3_vars[3] = p3->Properties.Light1;  
+  }
+  if (p->N >= 4) {
+	p1_vars[4] = p1->Properties.Light2;
+	p2_vars[4] = p2->Properties.Light2;
+	p3_vars[4] = p3->Properties.Light2;  
+  }
+
     // Calculamos las deltas de todas la variables
+  
   for(i=0;i<=p->N;i++)
   {
-    sp.SPVars[i*2+1]= ancho*(temp * (p3->SpanVars.k[i] - p1->SpanVars.k[i]) + (p1->SpanVars.k[i] - p2->SpanVars.k[i]));
-    ks1[i]=   65536.0 * p1->SpanVars.k[i];
+    sp.SPVars[i*2+1]= ancho*(temp * (p3_vars[i] - p1_vars[i]) + (p1_vars[i] - p2_vars[i]));
+    ks1[i]=   65536.0 * p1_vars[i];
   }
   sp.SPN=p->N+1;
   sp.SPType=p->Type;
@@ -106,7 +137,7 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
         temp*=65536.0;
         for(i=0;i<=p->N;i++)
         {
-          deltas1[i]= temp * (p2->SpanVars.k[i] - p1->SpanVars.k[i]);
+          deltas1[i]= temp * (p2_vars[i] - p1_vars[i]);
         }
         x1=p1->ImageProjection.x;
         y=(int)p1y;
@@ -167,7 +198,7 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
       {
         for(i=0;i<=p->N;i++)
         {
-          ks1[i]=   65536.0 * p2->SpanVars.k[i];
+          ks1[i]=   65536.0 * p2_vars[i];
         }
       } */
       if((p3y - p2y)==0) return;
@@ -177,12 +208,12 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
       temp*=65536.0;
       for(i=0;i<=p->N;i++)
       {
-        deltas1[i]= temp * (p3->SpanVars.k[i] - p2->SpanVars.k[i]);
+        deltas1[i]= temp * (p3_vars[i] - p2_vars[i]);
       }
       x1=p2->ImageProjection.x;
 /*      for(i=0;i<=p->N;i++)
       {
-        ks1[i]=   65536.0 * p2->SpanVars.k[i]; pasado a abajo
+        ks1[i]=   65536.0 * p2_vars[i]; pasado a abajo
       }*/
 
       y=(int)p2y;
@@ -196,7 +227,7 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
       //x2+=((float)y-p2->ImageProjection.y)*dx2;
       for(i=0;i<=p->N;i++)
       {
-        ks1[i]=65536.0 * p2->SpanVars.k[i]+((float)y-p2->ImageProjection.y)*deltas1[i];
+        ks1[i]=65536.0 * p2_vars[i]+((float)y-p2->ImageProjection.y)*deltas1[i];
       }
       y2=(int)p3y;
       if(y2>sbuffer->SLIClip.SLRR2.SLPY) y2=sbuffer->SLIClip.SLRR2.SLPY;
@@ -233,7 +264,7 @@ extern void GenericTriangleMapper(SLI *sbuffer, RENDERPOLY *p)
       temp*=65536.0;
       for(i=0;i<=p->N;i++)
       {
-        deltas1[i]= temp * (p3->SpanVars.k[i] - p1->SpanVars.k[i]);
+        deltas1[i]= temp * (p3_vars[i] - p1_vars[i]);
       }
       dx3=(p3->ImageProjection.x - p2->ImageProjection.x)/(p3->ImageProjection.y - p2->ImageProjection.y);
 
